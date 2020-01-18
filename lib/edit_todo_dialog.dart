aa31f64b212eb8d0c07cc9416655e4d9fdbad541
maejimayuto <maejimayuto@gmail.com>
2020-01-19 03:14:50 +0900

    fill todo value

--- lib/edit_todo_dialog.dart
+++ lib/edit_todo_dialog.dart
@@ -3,16 +3,17 @@ import 'package:flutter/material.dart';
 import 'package:todo_flutter/todo.dart';
 
 class EditTodoDialog extends StatelessWidget {
-  final controller = new TextEditingController();
+  final TextEditingController controller = new TextEditingController();
   final Todo todo;
 
-  EditTodoDialog({@required this.todo});
+  EditTodoDialog({@required this.todo}) {
+    this.controller.text = todo.title;
+  }
 
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
       title: Text('Edit todo'),
-      // 初期値のバインドをしたい
       content: TextField(
         controller: controller,
         autofocus: true,
@@ -21,15 +22,12 @@ class EditTodoDialog extends StatelessWidget {
         FlatButton(
           child: Text('Cancel'),
           onPressed: () {
-            // コンストラクタで初期化された時の todo を返したい
             Navigator.of(context).pop(todo);
           },
         ),
         FlatButton(
           child: Text('Edit'),
           onPressed: () {
-            // これで合っているのか知らんが、
-            // 編集後の todo を返したい
             todo.title = controller.value.text;
             controller.clear();
 
