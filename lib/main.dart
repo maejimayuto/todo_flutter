add885d20b827da92436717852a641660a98347d
maejimayuto <maejimayuto@gmail.com>
2020-01-20 00:59:10 +0900

    start
    https://medium.com/flutterpub/flutter-how-to-do-user-login-with-firebase-a6af760b14d5

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,16 @@
 import 'package:flutter/material.dart';
+import 'login_signup_page.dart';
 
-import 'package:todo_flutter/todo_list_screen.dart';
-
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Todo List',
-      home: TodoListScreen(),
-    );
+    return new MaterialApp(
+        title: 'Flutter Login Demo',
+        theme: new ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: new LoginSignupPage());
   }
 }
